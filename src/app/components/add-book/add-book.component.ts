57789cbc4fccb0aaf36db7ae17334bd9b384b2c2
Olga <alter.koval.olga@gmail.com>
2023-03-08 18:43:54 +0100

    add new book form

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Book } from '@models/book.interface';
 import { CatalogueState } from '@models/catalogue-state.interface';
 import { Store } from '@ngrx/store';
+import { fourDigits, onlyLetters, yearInPast } from './add-book-form-validators';
 import { AddBookComponentActions } from './state/actions';
 
 @Component({
   selector: 'app-add-book',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './add-book.component.html',
   styleUrls: ['./add-book.component.css']
 })
@@ -17,19 +19,22 @@ export class AddBookComponent {
   }
 
   public form = new FormGroup({
-    title: new FormControl('', { nonNullable: true }),
-    author: new FormControl('', { nonNullable: true }),
-    year: new FormControl('', { nonNullable: true }),
+    title: new FormControl('', { nonNullable: true, validators: [Validators.required, onlyLetters()] }),
+    author: new FormControl('', { nonNullable: true, validators: [Validators.required, onlyLetters()] }),
+    year: new FormControl('', { nonNullable: true, validators: [Validators.required, fourDigits(), yearInPast()] }),
   });
 
-  addBook() {
+  get title() { return this.form.get('title'); }
+  get author() { return this.form.get('author'); }
+  get year() { return this.form.get('year'); }
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
     const formValue: Book =
-    {
-      year: '2333',
-      title: 'title',
-      author: 'author'
-    };
-    //this.form.getRawValue();
+      this.form.getRawValue();
 
     this.store.dispatch(AddBookComponentActions.addBook({ book: formValue }));
   }
